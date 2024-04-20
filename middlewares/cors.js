67365b756ef18367ae8e3a2e562e0349e0ba3b04
kimchiur <149685962+kimchiur@users.noreply.github.com>
2024-04-20 17:26:50 +0300

    first of all

---
+++ middlewares/cors.js
@@ -0,0 +1,11 @@
+function cors(req, res, next) {
+	const { origin } = req.headers;
+	const allowedCors = ['https://practicum.yandex.ru', 'https://students-projects.ru', 'http://localhost:3000'];
+
+	if (allowedCors.includes(origin)) {
+		// Если это наш друг
+		res.header('Access-Control-Allow-Origin', origin);
+	}
+	next();
+}
+module.exports = cors;
