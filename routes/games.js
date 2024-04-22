a92f1bc38ee589e29fd68167cbe1c9d3bd935831
dashenk <dakorotria@gmail.com>
2024-04-22 17:16:50 +0300

    bruh

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,19 @@
-// routes/games.js
-const gamesRouter = require('express').Router(); // Создали роутер
-const { sendAllGames, deleteGame, addGameController } = require('../controllers/games');
-const { getAllGames } = require('../middlewares/games');
+const gamesRouter = require('express').Router();
+
+const {
+	getAllGames,
+	checkIsTitleInArray,
+	updateGamesArray,
+	updateGamesFile,
+	findGameById,
+	deleteGame
+} = require('../middlewares/games');
+const { sendAllGames, sendUpdatedGames } = require('../controllers/games');
 
 gamesRouter.get('/games', getAllGames, sendAllGames);
-gamesRouter.delete('/games/:id', getAllGames, deleteGame);
-gamesRouter.post('/games', getAllGames, addGameController);
+
+gamesRouter.post('/games', getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
+
+gamesRouter.delete('/games/:id', getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
 
 module.exports = gamesRouter;
