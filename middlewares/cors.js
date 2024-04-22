d457b6fddebcf925b1d7803e039d4f773e6c4cc6
dashenk <dakorotria@gmail.com>
2024-04-22 17:08:51 +0300

    mew

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,15 @@
-function cors(req, res, next) {
-	const { origin } = req.headers;
-	const allowedCors = ['https://practicum.yandex.ru', 'https://students-projects.ru', 'http://localhost:3000'];
+const allowedCors = ['https://practicum.yandex.ru', 'https://students-projects.ru', 'http://localhost:3000'];
 
+// Функция, которая принимает объекты req (информация о запросе),
+// res (объект ответа) и функцию next (для запуска следующего миддлвара)
+function cors(req, res, next) {
+	const { origin } = req.headers; // Смотрим, кто стучится к серверу
 	if (allowedCors.includes(origin)) {
-		// Если это наш друг
-		res.header('Access-Control-Allow-Origin', origin);
+		// Если это наш друг,
+		res.header('Access-Control-Allow-Origin', origin); // говорим: «Добро пожаловать!»
 	}
-	next();
+
+	next(); // Идём дальше, не задерживаем очередь
 }
+
 module.exports = cors;
